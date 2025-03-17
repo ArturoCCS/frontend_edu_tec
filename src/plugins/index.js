44d46b615c8964ba4c7ef0e5a09e1f910baed66c
ArturoCCS <ccatschark@gmail.com>
2025-03-16 20:18:03 -0600

    Modificacion src/plugins/index.js para mejor manejo de creacion de la app

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -6,10 +6,18 @@
 
 // Plugins
 import router from '@/router'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import store from './store'
 import vuetify from './vuetify'
 
-export function registerPlugins (app) {
+export function registerPlugins(App) {
+  const app = createApp(App)
+
+  app.use(createPinia()) 
   app
     .use(vuetify)
     .use(router)
+    .use(store)
+    .mount('#app')
 }
