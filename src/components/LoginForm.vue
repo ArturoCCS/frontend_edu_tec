e9af1ed7a167bbe4b11609ad1e549cc74d511f49
ArturoCCS <ccatschark@gmail.com>
2025-03-17 13:50:36 -0600

    Cambio en la presentacion de mensajes de error

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,43 +1,57 @@
 <template>
   <v-row class="mt-2 mb-2" justify="center">
-    <v-form class="form" @submit.prevent="handleLogin">
-      <v-img class="img" src="/petirrojo.jpg" cover />
+    <div class="form">
+      <v-form @submit.prevent="handleLogin">
+        <v-img class="img" src="/petirrojo.jpg" cover />
 
-      <div class="flex-column">
-        <v-label>Email</v-label>
-      </div>
-      <div class="inputForm">
-        <input v-model="email" type="text" class="input" placeholder="Enter your Email">
-      </div>
+        <span v-if="mensaje" class="error-message">{{ mensaje }}</span>
 
-      <div class="flex-column">
-        <v-label>Password</v-label>
-      </div>
-      <div class="inputForm">
-        <input v-model="password" type="password" class="input" placeholder="Enter your Password">
-      </div>
+        <div :class="{ 'inputForm': true, 'error': emailError }">
+          <i class="icon-form fa-solid fa-envelope"></i>
+          <input v-model="email" type="text" class="input" required>
+          <div class="label-line">Enter your email</div>
+          <i v-if="emailError" class="error-icon fa-solid fa-triangle-exclamation"></i>
+        </div>
+        <span v-if="emailError" class="error-message">{{ emailError }}</span>
+
+        <div :class="{ 'inputForm': true, 'error': passwordError }">
+          <i class="icon-form fa-solid fa-lock icon"></i>
+          <i class="password fa-regular" :class="passwordVisible ? 'fa-eye-slash' : 'fa-eye'"
+            @click="togglePasswordVisibility()"></i>
+          <input v-model="password" :type="passwordVisible ? 'text' : 'password'" class="input" required>
+          <div class="label-line">Enter your password</div>
+          <i v-if="passwordError" class="error-icon fa-solid fa-triangle-exclamation"></i>
+        </div>
+        <span v-if="passwordError" class="error-message">{{ passwordError }}</span>
+
+
+        <div class="flex-row">
+          <span class="span">¿Olvidó su contraseña?</span>
+        </div>
+
+        <button class="button-submit">Iniciar sesión</button>
+        <p class="p">¿No tienes una cuenta?
+          <a href="/register" class="span" style="text-decoration: none;">
+            Regístrate
+          </a>
+        </p>
+        <br>
+        <p class="p line">O</p>
+
+      </v-form>
 
       <div class="flex-row">
-        <span class="span">¿Olvidó su contraseña?</span>
+        <button class="btn google" @click="withGoogle">
+          <img src="../assets/google.svg" alt="">
+          Google
+        </button>
+        <button class="btn google" @click="withMicrosoft">
+          <img src="../assets/microsoft.svg" alt="">
+          Microsoft
+        </button>
       </div>
 
-      <button class="button-submit">Iniciar sesión</button>
-                <p class="p">¿No tienes una cuenta? 
-                  <a href="/register" class="span" style="text-decoration: none;">Regístrate</a></p>
-                <p class="p line">O</p> 
-
-                <div class="flex-row">
-                    <button class="btn google" @click="withGoogle">
-                        <img src="../assets/google.svg" alt="">
-                        Google 
-                    </button>
-                    <button class="btn google" @click="withMicrosoft">
-                        <img src="../assets/microsoft.svg" alt="">
-                        Microsoft 
-                    </button>
-                </div>
-      <p class="p">{{ mensaje }}</p>
-    </v-form>
+    </div>
   </v-row>
 </template>
 
@@ -51,23 +65,64 @@ export default {
     const authStore = useAuthStore()
     const email = ref('')
     const password = ref('')
+    const passwordVisible = ref(false)
     const mensaje = ref('')
+    const emailError = ref('')
+    const passwordError = ref('')
     const router = useRouter()
 
     const handleLogin = async () => {
-      const response = await authStore.login({ email: email.value, password: password.value })
+      emailError.value = "";
+      passwordError.value = "";
+      mensaje.value = "";
 
-      mensaje.value = response.message
+      let hasError = false;
 
-      if (response.status == "success") {
-        router.push('/protected_route')
+      if (!email.value) {
+        emailError.value = "El correo es obligatorio.";
+        hasError = true;
       }
-    }
+      if (!password.value) {
+        passwordError.value = "La contraseña es obligatoria.";
+        hasError = true;
+      }
+
+      if (hasError) {
+        return;
+      }
+
+      const response = await authStore.login({ email: email.value, password: password.value });
+
+      if (response.status === "error") {
+        mensaje.value = response.message;
+
+        if (response.message.includes("Correo")) {
+          emailError.value = response.message;
+        }
+        if (response.message.includes("contraseña")) {
+          passwordError.value = response.message;
+        }
+        return;
+      }
+
+      if (response.status === "success") {
+        router.push("/protected_route");
+      }
+    };
+
+
+    const togglePasswordVisibility = () => {
+      passwordVisible.value = !passwordVisible.value;
+    };
 
     return {
       email,
       password,
       mensaje,
+      emailError,
+      passwordError,
+      passwordVisible,
+      togglePasswordVisibility,
       handleLogin,
       withGoogle: authStore.withGoogle,
       withMicrosoft: authStore.withMicrosoft
@@ -77,5 +132,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@use '@/styles/main.scss';    
+@use '@/styles/main.scss';
 </style>
