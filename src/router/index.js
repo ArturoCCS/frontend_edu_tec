2dd291fd319cb5300f5fb1ad2bd49e120282291c
ArturoCCS <ccatschark@gmail.com>
2025-10-29 22:56:17 -0600

    Cambios para verificar permiso index.js

--- src/router/index.js
+++ src/router/index.js
@@ -7,42 +7,27 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
-
 router.beforeEach(async (to, from, next) => {
   const authStore = useAuthStore();
   const userStore = useUserStore();
 
   if (to.meta.requiresAuth) {
-    if (!authStore.token) {
-      try {
-        const authStatus = await authStore.checkAuth();
-        if (authStatus.status !== 'success') {
-          return next('/login');
-        }
-      } catch (error) {
-        console.error("Error al verificar autenticación", error);
-        return next('/login');
-      }
+    if (!authStore.user) {
+      const authStatus = await authStore.checkAuth();
+      if (authStatus.status !== 'success') return next('/login');
     }
 
     if (to.meta.requiresPermission) {
-      try {
-        if (userStore.permissions.length === 0) {
-          userStore.user = authStore.user;
-          const userPermission = await userStore.checkPermission();
+      const user = authStore.user;
+      if (user.id_rol === Number(import.meta.env.VITE_APP_ROL_ADMIN)) return next();
 
-          if (userPermission.status !== 'success') {
-            return next('/'); 
-          }
-        }
-      } catch (error) {
-        console.error("Error al verificar permisos", error);
-        return next('/'); 
-      }
+      const hasPermission = userStore.permissions.includes(to.meta.permission);
+      if (!hasPermission) return next('/'); 
     }
   }
 
   next();
 });
+
 
 export default router;
